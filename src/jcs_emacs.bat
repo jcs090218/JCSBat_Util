d43cf294a934cfc2261db81d14f969fb11969f70
jenchieh <jcs090218@gmail.com>
2017-11-15 12:03:02 +0800

    default open emacs with emacs init file.

---
+++ src/jcs_emacs.bat
@@ -0,0 +1,17 @@
+@echo off
+:: ========================================================================
+:: $File: jcs_emacs.bat $
+:: $Date: 2017-11-15 11:57:51 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright (c) 2017 by Shen, Jen-Chieh $
+:: ========================================================================
+
+
+set EMACS_INIT_PATH=
+set EMACS_PROGRAM_PATH=
+
+
+:: Run emacs with initialize setting file.
+%EMACS_PROGRAM_PATH% %EMACS_INIT_PATH%
