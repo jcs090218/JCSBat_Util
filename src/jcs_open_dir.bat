b250a474c87c56d757643bcc534f521176555b6a
jenchieh <jcs090218@gmail.com>
2018-06-25 23:07:14 +0800

    Use jcs_open_bk instead of the same/duplicate code.

--- src/jcs_open_dir.bat
+++ src/jcs_open_dir.bat
@@ -9,18 +9,7 @@
 :: ========================================================================
 
 
-:: ------------------------------------------------------------------------
-:: How to use this?
-:: ------------------------------------------------------------------------
-:: open the backup directory depends on the hard drive applied.
-set BACK_UP_DRIVE=G:\
-:: ------------------------------------------------------------------------
-
-set CURRENT_PATH=%CD%
-set CURRENT_PATH_WITHOUT_DRIVE_LETTER=%CD:~3%
-
-set BACK_UP_PATH=%BACK_UP_DRIVE%%CURRENT_PATH_WITHOUT_DRIVE_LETTER%
-
-explorer %BACK_UP_PATH%
+start jcs_open_bk G
+start jcs_open_bk S
 
 exit /b
--- src/jcs_open_dir_reverse.bat
+++ src/jcs_open_dir_reverse.bat
@@ -15,14 +15,7 @@
 :: ------------------------------------------------------------------------
 :: open the backup directory depends on the hard drive applied.
 :: C drive is the default windows's OS system drive.
-set BACK_UP_DRIVE=C:\
+start jcs_open_bk C
 :: ------------------------------------------------------------------------
-
-set CURRENT_PATH=%CD%
-set CURRENT_PATH_WITHOUT_DRIVE_LETTER=%CD:~3%
-
-set BACK_UP_PATH=%BACK_UP_DRIVE%%CURRENT_PATH_WITHOUT_DRIVE_LETTER%
-
-explorer %BACK_UP_PATH%
 
 exit /b
